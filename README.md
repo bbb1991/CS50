a34e64fd8abf94c340915452f050ee77cc37ded8
Bagdat Bimaganbetov <bagdat.bimaganbetov@gmail.com>
2015-08-13 23:47:16 +0600

    Begin watch week 2

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 Данный проект предназначен для домашних работ по курсу Computer Science от HarvardX, на платформе EdX
+ALERT. This project have spoilers. DO NOT OPEN this project, if you learning/studying/reading/watching or whatever... CS50x
--- week_1/build.sh
+++ week_1/build.sh
@@ -1,3 +1,9 @@
+clang -ggdb -v -Wall -Werror $1 && ./a.out
 
+# -W -means show warnings (in case, -Wall means, show all warning, like "show unusual var, etc...)
+# -Werror - it means, if -Wall generate warning, doesn't matter what kind, we will not continue compile
+# also, we can compile with gcc:
+# gcc hello.c -o hello
+# or with make:
+# make hello
 
-gcc $1 && ./a.out
